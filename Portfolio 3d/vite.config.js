b4f09a371d67e622b3f49679240d93bbf58bae58
Rohan Khanna <rk868@njit.edu>
2025-04-15 12:03:11 -0400

    Changed base dir

--- Portfolio 3d/vite.config.js
+++ Portfolio 3d/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Portfolio-3D/',
   plugins: [
     react(),
     tailwindcss(),
